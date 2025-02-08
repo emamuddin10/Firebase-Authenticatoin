07ba85bc2b5cf0f2ef43b32907d49e71468e2e09
Emam Uddin <emamskingdom@gmail.com>
2025-02-08 21:00:16 +0600

    first commit

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+    const handlelogin = e =>{
+        e.preventDefault()
+        console.log(e)
+        const email = e.target.email
+        const password = e.target.password
+
+        console.log(email,password)
+    }
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <h1 className="text-5xl font-bold text-center p-10">Login now!</h1>
+          <form onSubmit={handlelogin} className="card-body">
+            <fieldset className="fieldset">
+              <label className="fieldset-label">Email</label>
+              <input type="email" name="email" className="input" placeholder="Email" />
+              <label className="fieldset-label">Password</label>
+              <input type="password" name="password" className="input" placeholder="Password" />
+              <div>
+                <a className="link link-hover">Forgot password?</a>
+              </div>
+              <button className="btn btn-neutral mt-4">Login</button>
+            </fieldset>
+            <p>
+                If you new this website please <button className="btn"><Link to='/register'>Register</Link></button>
+            </p>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/components/Register/Register.jsx
@@ -0,0 +1,39 @@
+
+import { Link } from "react-router-dom";
+
+const Register = () => {
+    const handleRegister = e =>{
+        e.preventDefault()
+        const name = e.target.name.value 
+        const email = e.target.email.value 
+        const password = e.target.password.value 
+
+        console.log(name,email,password)
+    }
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <h1 className="text-5xl font-bold text-center p-10">Register now!</h1>
+          <form onSubmit={handleRegister} className="card-body">
+            <fieldset className="fieldset">
+              <label className="fieldset-label">Name</label>
+              <input type="text" name="name" className="input" placeholder="Name" />
+              <label className="fieldset-label">Email</label>
+              <input type="email" name="email" className="input" placeholder="Email" />
+              <label className="fieldset-label">Password</label>
+              <input type="password" name="password" className="input" placeholder="Password" />
+              
+              <button className="btn btn-success mt-4">Login</button>
+            </fieldset>
+            <p>
+                If you have already account please <button className="btn"><Link to='/login'>Login</Link></button>
+            </p>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
