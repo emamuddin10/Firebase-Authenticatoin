6e111ef617f00ecbf6c79d005190d4ae7cef7c7a
Emam Uddin <emamskingdom@gmail.com>
2025-02-09 13:46:35 +0600

    aadded profile

---
+++ src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div>
+            <h1>Hhis is privet route</h1>
+            <p>you cant see this page without login</p>
+        </div>
+    );
+};
+
+export default Profile;
